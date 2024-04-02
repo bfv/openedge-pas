b643fa541b69d056941f7f019ab43f8bbea4d600
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2024-04-02 14:46:57 +0200

    fix prod CI build

--- start-prod-ci.sh
+++
@@ -1,7 +0,0 @@
-
-/app/pas/as/bin/oeprop.sh pas.ROOT.WEB.adapterEnabled=1
-echo "\"/web/**\",\"*\",\"permitAll()\"" > /app/pas/as/webapps/ROOT/WEB-INF/oeablSecurity.csv
-
-cat /app/pas/as/webapps/ROOT/WEB-INF/oeablSecurity.csv
-
-/app/pas/start.sh
